95d6db92967a81d8d110276ae4572582145f662a
woOter89 <flag005@gmail.com>
2020-02-25 12:43:04 +0100

    fix build

--- src/app.ts
+++ src/app.ts
@@ -2,16 +2,17 @@ import express, { Application } from 'express';
 
 import { LOG_MESSAGES, PORT } from './constants';
 import { dbConnect } from './data-access';
+import { logger } from './middlewares/logger';
 import rootRouter from './routes';
 import { ROUTES } from './routes/constants';
 
 const app: Application = express();
 
 app.use(express.json());
-app.use(ROUTES.userRoutes.root, rootRouter);
+app.use(rootRouter);
 
 app.listen(PORT, async () => {
-    console.log(`${ LOG_MESSAGES.portListening } ${ PORT }`);
+    logger.info(`${ LOG_MESSAGES.portListening } ${ PORT }`);
     try {
         await dbConnect();
     } catch (e) {
