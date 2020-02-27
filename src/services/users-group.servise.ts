7793037c90e1ff17203eda14cf9770ce2e9175ea
woOter89 <flag005@gmail.com>
2020-02-27 10:36:38 +0100

    A bit refactoring

--- src/services/users-group.servise.ts
+++ src/services/users-group.servise.ts
@@ -1,4 +1,4 @@
-import { usersGroupDAL } from '../data-access/dal/user-group.DAL';
+import { usersGroupDAL } from '../data-access';
 
 const addUsersToGroup = async (group_id: string, user_ids: string[]): Promise<any> =>
     await usersGroupDAL.addUsersToGroup(group_id, user_ids);
